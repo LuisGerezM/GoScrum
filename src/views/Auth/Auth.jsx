d32c8d1c3002fcd17e2e4c59e7f798708a5fcfef
LuisGerezM <luis.gerez@gmail.com>
2022-05-21 00:34:46 -0300

    terminado check de sesion de usuario

--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -1,19 +1,26 @@
-import Form from "./components/Form/Form"
 import { useUserForm } from "hooks/useAuthUser/useAuthUserForm"
 import { useAuthData } from "hooks/useAuthUser/useAuthData"
-import { motion } from "framer-motion"
+import { utilCheckSession } from "utilities/utilAuthUser/utilCheckSession/utilCheckSession"
 
+import Form from "./components/Form/Form"
+import { Navigate } from "react-router-dom"
 import { SpinnerLoad } from "components/Loading/SpinnerLoad/SpinnerLoad"
 import { alertMsg } from "utilities/utilAlert/utilAlertMsg"
+
+import { motion } from "framer-motion"
 
 import "./Auth.styles.css"
 
 export const Auth = ({ pageTransition }) => {
   const { loadingUser, pathName } = useUserForm()
 
+  // register
   const { authData, authDataError } = useAuthData()
 
   if (authDataError && pathName === "register") return alertMsg({ title: "ERROR", text: "Ups... Ocurrió un problema", icon: "error" })
+
+  // login
+  if (utilCheckSession(pathName).status_t) return <Navigate to="/" replace={true} />
 
   return (
     <>
